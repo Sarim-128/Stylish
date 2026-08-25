f45566a63e21b2011e3e5cbad15f141b78139a71
Sarim_Hussain <sarimhussain557@gmail.com>
2026-08-25 19:58:06 +0500

    Structured Home Page

--- App.tsx
+++ App.tsx
@@ -9,6 +9,26 @@ import Onboarding2 from './src/screens/Pre_Login/Onboarding2'
 import Onboarding3 from './src/screens/Pre_Login/Onboarding3'
 import Signin from './src/screens/Pre_Login/Signin'
 import Signup from './src/screens/Pre_Login/Signup'
+import { createNativeBottomTabNavigator } from '@react-navigation/bottom-tabs/unstable'
+import HomePage from './src/screens/Post_Login/HomePage'
+import Wishlist from './src/screens/Post_Login/Wishlist'
+import Cart from './src/screens/Post_Login/Cart'
+import Profile from './src/screens/Post_Login/Profile'
+
+
+
+const Tabs = createNativeBottomTabNavigator()
+const BottomTabs = () => {
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name='HomePage' component={HomePage} />
+      <Tabs.Screen name='Wishlist' component={Wishlist} />
+      <Tabs.Screen name='Cart' component={Cart} />
+      {/* <Tabs.Screen name='unknown' component={unknown} /> */}
+      <Tabs.Screen name='Profile' component={Profile} />
+    </Tabs.Navigator>
+  )
+}
 
 
 
@@ -23,8 +43,9 @@ const App = () => {
           <Stack.Screen name='Onboarding1' component={Onboarding1} />
           <Stack.Screen name='Onboarding2' component={Onboarding2} />
           <Stack.Screen name='Onboarding3' component={Onboarding3} /> 
-          <Stack.Screen name='Signin' component={Signin} /> */}
-          <Stack.Screen name='Signup' component={Signup} />
+          <Stack.Screen name='Signin' component={Signin} /> 
+          <Stack.Screen name='Signup' component={Signup} /> */}
+          <Stack.Screen name='BottomTabs' component={BottomTabs} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
--- src/screens/Post_Login/HomePage.tsx
+++ src/screens/Post_Login/HomePage.tsx
@@ -1,14 +1,100 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import MainInput from '../../components/MainInput'
 
 const HomePage = () => {
     return (
-        <View>
-            <Text>HomePage</Text>
-        </View>
+        <SafeAreaView>
+
+            {/* HEADER */}
+            <View>
+                <TouchableOpacity>
+                    <Image style={styles.menuBtnIcon} source={require('../../assets/images/Home/menu.png')} />
+                </TouchableOpacity>
+
+                <View>
+                    <Image source={require('../../assets/images/Home/logo.png')} />
+                    <Text>Stylish</Text>
+                </View>
+
+                <TouchableOpacity>
+                    <Image style={styles.profilePicBtnIcon} />
+                </TouchableOpacity>
+            </View>
+
+
+            {/* SEARCH BAR */}
+            <MainInput
+                text='Search any product...'
+            />
+
+
+            {/* CATEGORY SECTION */}
+
+            {/* SORTING & FILTER */}
+            <View>
+                <Text>All Featured</Text>
+                <TouchableOpacity>
+                    <Text>Sort</Text>
+                    <Image style={styles.filterIcons} source={require('../../assets/images/Home/sort.png')} />
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text>Filter</Text>
+                    <Image style={styles.filterIcons} source={require('../../assets/images/Home/filter.png')} />
+                </TouchableOpacity>
+            </View>
+
+            <View>
+                <TouchableOpacity>
+                    <Image />
+                    <Text>Beauty</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Image />
+                    <Text>Fashion</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Image />
+                    <Text>Kids</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Image />
+                    <Text>Mens</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Image />
+                    <Text>Womens</Text>
+                </TouchableOpacity>
+            </View>
+
+
+
+            {/* BANNERS */}
+            <View>
+
+            </View>
+
+
+            {/* FEED */}
+        </SafeAreaView>
     )
 }
 
 export default HomePage
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    menuBtnIcon: {
+        height: 25,
+        width: 25
+    },
+    filterIcons: {
+        width: 25,
+        height: 25,
+    }
+})
