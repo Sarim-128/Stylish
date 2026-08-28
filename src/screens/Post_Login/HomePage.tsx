054d0f934e3457f897d9571f9c5f193a1e53c040
Sarim_Hussain <sarimhussain557@gmail.com>
2026-08-28 17:07:46 +0500

    Structured Home Screen

--- src/screens/Post_Login/HomePage.tsx
+++ src/screens/Post_Login/HomePage.tsx
@@ -1,11 +1,26 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { FlatList, Image, NativeScrollEvent, NativeSyntheticEvent, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import MainInput from '../../components/MainInput'
 
+const banners = [
+    require('../../assets/images/Home/banner1.jpg'),
+    require('../../assets/images/Home/banner2.jpg'),
+    require('../../assets/images/Home/banner3.jpg'),
+]
+
+const ITEM_WIDTH = 360
 const HomePage = () => {
+    const [activeIndex, setActiveIndex] = useState(0)
+
+    const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+        const contentOffsetX = event.nativeEvent.contentOffset.x
+        const currentIndex = Math.round(contentOffsetX / ITEM_WIDTH)
+        setActiveIndex(currentIndex)
+
+    }
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
 
             {/* HEADER */}
             <View>
@@ -29,66 +44,98 @@ const HomePage = () => {
                 text='Search any product...'
             />
 
+            <ScrollView>
 
-            {/* CATEGORY SECTION */}
+                {/* CATEGORY SECTION */}
 
-            {/* SORTING & FILTER */}
-            <View>
-                <Text>All Featured</Text>
-                <TouchableOpacity>
-                    <Text>Sort</Text>
-                    <Image style={styles.filterIcons} source={require('../../assets/images/Home/sort.png')} />
-                </TouchableOpacity>
+                {/* SORTING & FILTER */}
+                <View>
+                    <Text>All Featured</Text>
+                    <TouchableOpacity>
+                        <Text>Sort</Text>
+                        <Image style={styles.filterIcons} source={require('../../assets/images/Home/sort.png')} />
+                    </TouchableOpacity>
 
-                <TouchableOpacity>
-                    <Text>Filter</Text>
-                    <Image style={styles.filterIcons} source={require('../../assets/images/Home/filter.png')} />
-                </TouchableOpacity>
-            </View>
+                    <TouchableOpacity>
+                        <Text>Filter</Text>
+                        <Image style={styles.filterIcons} source={require('../../assets/images/Home/filter.png')} />
+                    </TouchableOpacity>
+                </View>
 
-            <View>
-                <TouchableOpacity>
-                    <Image />
-                    <Text>Beauty</Text>
-                </TouchableOpacity>
+                <View>
+                    <TouchableOpacity>
+                        <Image />
+                        <Text>Beauty</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity>
-                    <Image />
-                    <Text>Fashion</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image />
+                        <Text>Fashion</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity>
-                    <Image />
-                    <Text>Kids</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image />
+                        <Text>Kids</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity>
-                    <Image />
-                    <Text>Mens</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image />
+                        <Text>Mens</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity>
-                    <Image />
-                    <Text>Womens</Text>
-                </TouchableOpacity>
-            </View>
-
-
-
-            {/* BANNERS */}
-            <View>
-
-            </View>
+                    <TouchableOpacity>
+                        <Image />
+                        <Text>Womens</Text>
+                    </TouchableOpacity>
+                </View>
 
 
-            {/* FEED */}
-        </SafeAreaView>
+
+                {/* BANNERS */}
+                <View>
+                    <ScrollView style={styles.bannerContainer}
+                        horizontal={true}
+                        showsHorizontalScrollIndicator={false}
+                        snapToInterval={360}
+                        decelerationRate='fast'
+                        onScroll={handleScroll}
+                        scrollEventThrottle={16}
+                    >
+                        <Image style={styles.bannerImg} source={require('../../assets/images/Home/banner1.jpg')} />
+                        <Image style={styles.bannerImg} source={require('../../assets/images/Home/banner2.jpg')} />
+                        <Image style={styles.bannerImg} source={require('../../assets/images/Home/banner3.jpg')} />
+                    </ScrollView>
+
+                    {/* CIRCLE INDICATORS */}
+                    <View style={styles.paginationContainer}>
+                        {banners.map((_, index) => (
+                            <View
+                                key={index}
+                                style={[
+                                    styles.dot,
+                                    activeIndex === index ? styles.activeDot : styles.inactiveDot,
+                                ]}
+                            />
+                        ))}
+                    </View>
+                </View>
+
+
+
+                {/* FEED */}
+
+            </ScrollView>
+        </SafeAreaView >
     )
 }
 
 export default HomePage
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF'
+    },
     menuBtnIcon: {
         height: 25,
         width: 25
@@ -96,5 +143,42 @@ const styles = StyleSheet.create({
     filterIcons: {
         width: 25,
         height: 25,
-    }
+    },
+    bannerContainer: {
+        flexDirection: 'row',
+        flex: 1,
+    },
+    bannerImg: {
+        width: 320,
+        height: 180,
+        borderRadius: 10,
+        marginHorizontal: 20,
+        boxShadow: [
+            {
+                offsetX: 0,
+                offsetY: 3,
+                blurRadius: 4,
+                color: 'rgba(0,0,0,0.1)',
+            },
+        ],
+    },
+    paginationContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 8,
+    },
+    dot: {
+        height: 8,
+        borderRadius: 4,
+        marginHorizontal: 4,
+    },
+    activeDot: {
+        width: 24,
+        backgroundColor: '#FFA3B3',
+    },
+    inactiveDot: {
+        width: 8,
+        backgroundColor: '#E0E0E0',
+    },
 })
--- src/screens/Post_Login/Profile.tsx
+++ src/screens/Post_Login/Profile.tsx
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView>
+      <Text>Checkout</Text>
+
+
+      {/* PROFILE PIC */}
+      <View>
+        <Image />
+        <TouchableOpacity>
+          <Image />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
