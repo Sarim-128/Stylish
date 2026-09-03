89562b91b35a7453e14075f20b0d601d39f3fd5a
Sarim_Hussain <sarimhussain557@gmail.com>
2026-09-04 00:07:20 +0500

    Styled Browsing feed & added functionality to sort button

--- src/screens/Post_Login/HomePage.tsx
+++ src/screens/Post_Login/HomePage.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, FlatList, Image, NativeScrollEvent, NativeSyntheticEvent, RefreshControl, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, FlatList, Image, Modal, NativeScrollEvent, NativeSyntheticEvent, RefreshControl, ScrollView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import MainInput from '../../components/MainInput'
@@ -63,6 +63,15 @@ const HomePage = ({ navigation }: any) => {
         );
     }
 
+    // HANDLE SORTING
+    const [selectedCategory, setSelectedCategory] = useState<string>('all')
+
+    const categories = ['all', 'beauty', 'fragnances', 'furniture']
+
+    const filterProducts = selectedCategory === 'all' ? products
+        : products.filter((p: any) => p.category?.toLowerCase() === selectedCategory.toLowerCase())
+
+    const [sortModalVisible, setSortModalVisible] = useState(false)
 
 
 
@@ -70,7 +79,7 @@ const HomePage = ({ navigation }: any) => {
 
         <SafeAreaView style={styles.container}>
 
-            <StatusBar />
+            <StatusBar barStyle='dark-content' />
 
             {/* HEADER */}
             <View style={styles.headerContainer}>
@@ -109,7 +118,10 @@ const HomePage = ({ navigation }: any) => {
                     <Text style={styles.categoryHeaderTitle}>All Featured</Text>
 
                     <View style={styles.categoryBtnsContainer}>
-                        <TouchableOpacity style={styles.sortContainer}>
+                        <TouchableOpacity
+                            style={styles.sortContainer}
+                            onPress={() => setSortModalVisible(true)}
+                        >
                             <Text style={styles.sortTitle}>Sort</Text>
                             <Image style={styles.sortIcon} source={require('../../assets/images/Home/sort.png')} />
                         </TouchableOpacity>
@@ -123,42 +135,26 @@ const HomePage = ({ navigation }: any) => {
 
 
                 {/* SPECIFIC CATEGORIES */}
+
+
                 <View style={styles.specificCategoriesCard}>
-                    <ScrollView
-                        horizontal
-                        showsHorizontalScrollIndicator={false}
-                        contentContainerStyle={styles.specificCategoriesScrollContent}
-                    >
 
-                        <TouchableOpacity style={styles.SCContainer}>
-                            <Image style={styles.SCIcon} source={require('../../assets/images/Home/beauty.png')} />
-                            <Text style={styles.SCText}>Beauty</Text>
-                        </TouchableOpacity>
+                    <TouchableOpacity style={styles.SCContainer}>
+                        <Image style={styles.SCIcon} source={require('../../assets/images/Home/beauty.png')} />
+                        <Text style={styles.SCText}>Beauty</Text>
+                    </TouchableOpacity>
 
-                        <TouchableOpacity style={styles.SCContainer}>
-                            <Image style={styles.SCIcon} source={require('../../assets/images/Home/fashion.jpg')} />
-                            <Text style={styles.SCText}>Fashion</Text>
-                        </TouchableOpacity>
+                    <TouchableOpacity style={styles.SCContainer}>
+                        <Image style={styles.SCIcon} source={require('../../assets/images/Home/fragnance.jpg')} />
+                        <Text style={styles.SCText}>Fragnance</Text>
+                    </TouchableOpacity>
 
-                        <TouchableOpacity style={styles.SCContainer}>
-                            <Image style={styles.SCIcon} source={require('../../assets/images/Home/kids.jpg')} />
-                            <Text style={styles.SCText}>Kids</Text>
-                        </TouchableOpacity>
+                    <TouchableOpacity style={styles.SCContainer}>
+                        <Image style={styles.SCIcon} source={require('../../assets/images/Home/furniture.jpg')} />
+                        <Text style={styles.SCText}>Furniture</Text>
+                    </TouchableOpacity>
 
-                        <TouchableOpacity style={styles.SCContainer}>
-                            <Image style={styles.SCIcon} source={require('../../assets/images/Home/mens.jpg')} />
-                            <Text style={styles.SCText}>Mens</Text>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity style={styles.SCContainer}>
-                            <Image style={styles.SCIcon} source={require('../../assets/images/Home/womens.png')} />
-                            <Text style={styles.SCText}>Womens</Text>
-                        </TouchableOpacity>
-                    </ScrollView>
                 </View>
-
-
-
 
 
 
@@ -198,18 +194,68 @@ const HomePage = ({ navigation }: any) => {
                 {/* FEED */}
                 <FlatList
                     scrollEnabled={false}
-                    data={products}
+                    data={filterProducts}
                     numColumns={2}
                     keyExtractor={(item: any) => item.id.toString()}
+                    columnWrapperStyle={styles.rowWrapper}
+                    contentContainerStyle={styles.feedContainer}
+
                     renderItem={({ item }) => (
-                        <View>
-                            <Text>{item.title}</Text>
-                            <Text></Text>
-                        </View>
+                        <TouchableOpacity style={styles.cardWrapper}>
+                            <View style={styles.itemCard}>
+
+                                <View style={styles.imageContainer}>
+                                    <Image style={styles.itemImage} source={{ uri: item.images?.[0] }} />
+                                </View>
+
+                                <View style={styles.detailsContainer}>
+                                    <Text style={styles.itemTitle} numberOfLines={2}>{item.title}</Text>
+
+                                    <Text style={styles.itemPrice}>${item.price}</Text>
+
+
+                                    <View style={styles.ratingRow}>
+                                        <View style={styles.ratingBadge}>
+                                            <Text style={styles.ratingIcon}>★</Text>
+                                            <Text style={styles.ratingText}>{item.rating?.rate ?? item.rating ?? '4.5'}</Text>
+                                        </View>
+                                        <Text style={styles.stock}>Stock: {item.stock}</Text>
+                                    </View>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
                     )}
                 />
-
             </ScrollView>
+
+            {/* SORT MODAL */}
+            <Modal
+                visible={sortModalVisible}
+                transparent={true}
+                animationType='fade'
+                onRequestClose={() => setSortModalVisible(false)}
+            >
+                <TouchableOpacity
+                    style={styles.modalOverlay}
+                    activeOpacity={1}
+                    onPress={() => setSortModalVisible(false)}
+                >
+                    <View style={styles.modalContent}>
+                        <Text style={styles.modalTitle}>  Select Category</Text>
+                        {categories.map((cat) => (
+                            <TouchableOpacity
+                                key={cat}
+                                style={[styles.modalOption, selectedCategory === cat && styles.modalOptionSelected]}
+                                onPress={() => { setSelectedCategory(cat); setSortModalVisible(false) }}
+                            >
+                                <Text>
+                                    {cat === 'all' ? 'All Products' : cat.charAt(0).toUpperCase() + cat.slice(1)}
+                                </Text>
+                            </TouchableOpacity>
+                        ))}
+                    </View>
+                </TouchableOpacity>
+            </Modal>
         </SafeAreaView >
     )
 }
@@ -328,10 +374,14 @@ const styles = StyleSheet.create({
         height: 20,
     },
     specificCategoriesCard: {
+        gap: 30,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
         marginVertical: 20,
         backgroundColor: '#FFFFFF',
         borderRadius: 10,
-        padding: 12,
+        padding: 8,
         elevation: 2,
         boxShadow: [
             {
@@ -342,10 +392,6 @@ const styles = StyleSheet.create({
             },
         ],
     },
-    specificCategoriesScrollContent: {
-        alignItems: 'center',
-        gap: 15,
-    },
     SCContainer: {
         alignItems: 'center',
     },
@@ -355,7 +401,8 @@ const styles = StyleSheet.create({
         borderRadius: 30,
     },
     SCText: {
-
+        fontSize: 12,
+        fontFamily: 'Montserrat-Regular'
     },
     bannerContainer: {
         marginVertical: 20,
@@ -397,4 +444,128 @@ const styles = StyleSheet.create({
         width: 8,
         backgroundColor: '#E0E0E0',
     },
+
+
+    // FEED STYLE
+    rowWrapper: {
+        justifyContent: 'space-between',
+        marginBottom: 16
+    },
+    feedContainer: {
+        paddingVertical: 10,
+        paddingHorizontal: 2,
+    },
+    cardWrapper: {
+        width: '48%'
+    },
+    itemCard: {
+        backgroundColor: '#FFFFFF',
+        borderRadius: 16,
+        padding: 8,
+        elevation: 3,
+        shadowColor: '#000000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.08,
+        shadowRadius: 6,
+    },
+    imageContainer: {
+        width: '100%',
+        height: 140,
+        backgroundColor: '#F9F9F9',
+        borderRadius: 10,
+        overflow: 'hidden'
+    },
+    itemImage: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+    detailsContainer: {
+        paddingTop: 8,
+        paddingHorizontal: 2,
+    },
+    itemTitle: {
+        color: '#2E3E5C',
+        fontSize: 13,
+        fontFamily: 'Montserrat-Medium',
+        lineHeight: 18,
+        minHeight: 36,
+    },
+    itemPrice: {
+        color: '#000',
+        fontSize: 15,
+        fontFamily: 'Montserrat-SemiBold',
+        marginTop: 4
+    },
+    ratingRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginTop: 6,
+        gap: 4,
+    },
+    ratingBadge: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#fff4d9',
+        paddingHorizontal: 6,
+        paddingVertical: 2,
+        borderRadius: 6,
+        gap: 2,
+    },
+    ratingIcon: {
+        color: '#FFB800',
+        fontSize: 11,
+    },
+    ratingText: {
+        fontSize: 11,
+        fontFamily: 'Montserrat-SemiBold',
+        color: '#A06D00'
+    },
+    stock: {
+        fontSize: 10,
+        fontFamily: 'Montserrat-Regular',
+        color: '#808080',
+        marginRight: 4,
+    },
+
+    //MODAL STYLE
+    modalOverlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalContent: {
+        backgroundColor: '#FFFFFF',
+        width: '80%',
+        borderRadius: 12,
+        padding: 20,
+    },
+    modalTitle: {
+        fontSize: 16,
+        fontFamily: 'Montserrat-SemiBold',
+        marginBottom: 12,
+        textAlign: 'center',
+    },
+    modalOption: {
+        paddingVertical: 10,
+        paddingHorizontal: 12,
+        borderRadius: 6,
+        marginVertical: 4,
+    },
+    modalOptionSelected: {
+        backgroundColor: '#EBF3FE',
+    },
+    modalOptionText: {
+        fontSize: 14,
+        fontFamily: 'Montserrat-Regular',
+        color: '#333',
+    },
+    modalOptionTextSelected: {
+        fontSize: 14,
+        fontFamily: 'Montserrat-SemiBold',
+        color: '#4392F9',
+    },
+
 })
